7ef492ba8c89e262ec6fe5a447ab71b17d006f20
Dmitry Kretov <kretov_1996@bk.ru>
2024-04-20 12:23:21 +0300

    all numbers is now used in harpoon

--- init.lua
+++ init.lua
@@ -114,6 +114,15 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>6', function()
         harpoon:list():select(6)
       end)
+      vim.keymap.set('n', '<leader>7', function()
+        harpoon:list():select(7)
+      end)
+      vim.keymap.set('n', '<leader>8', function()
+        harpoon:list():select(8)
+      end)
+      vim.keymap.set('n', '<leader>9', function()
+        harpoon:list():select(9)
+      end)
     end,
   },
 
